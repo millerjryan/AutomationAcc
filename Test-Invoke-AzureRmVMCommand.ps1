3875d2c59e341c6812fe6d175d4a7e6832506b71
millerjryan <50216489+millerjryan@users.noreply.github.com>
2019-08-06 14:32:47 -0400

    Create Test-Invoke-AzureRmVMCommand.ps1

---
+++ Test-Invoke-AzureRmVMCommand.ps1
@@ -0,0 +1,43 @@
+################
+# Auth with Azure
+
+$connectionName = "AzureRunAsConnection"
+try
+{
+    # Get the connection "AzureRunAsConnection "
+    $servicePrincipalConnection=Get-AutomationConnection -Name $connectionName         
+
+    "Logging in to Azure..."
+    Add-AzureRmAccount `
+        -ServicePrincipal `
+        -TenantId $servicePrincipalConnection.TenantId `
+        -ApplicationId $servicePrincipalConnection.ApplicationId `
+        -CertificateThumbprint $servicePrincipalConnection.CertificateThumbprint 
+}
+catch {
+    if (!$servicePrincipalConnection)
+    {
+        $ErrorMessage = "Connection $connectionName not found."
+        throw $ErrorMessage
+    } else{
+        Write-Error -Message $_.Exception
+        throw $_.Exception
+    }
+}
+# End of Auth
+################
+
+$Logfile = ".\script.sh"
+
+Function LogWrite
+{
+   Param ([string]$logstring)
+
+   Add-content $Logfile -value $logstring
+}
+
+LogWrite "mkdir -p /home/vmadmin/testfolder/shared/db;"
+
+$files = Get-ChildItem
+Write-Output $files
+Invoke-AzVMRunCommand -ResourceGroupName "vjomiLABrg" -Name LNX-VM1-VJOMI -CommandId "RunShellScript" -ScriptPath .\script.sh
